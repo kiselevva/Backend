9aba516765f4b241c6964885dce50282e2cdbdea
kiselevva <80060751+kiselevva@users.noreply.github.com>
2022-06-07 19:43:42 +0300

    Add files via upload

---
+++ lw1/Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CalculatorLib;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ( args.Length < 1 )
+            {
+                Console.WriteLine( "Please enter calculation string" );
+            }
+            List<IOperation> operations = new()
+            {
+                new AdditionOperation(),
+                new SubstractionOperation(),
+                new MultiplicationOperation(),
+                new DivisionOperation()
+            };
+            ICalculator calculator = new SimpleCalculator( operations );
+            calculator.Calculate( args[0] );
+        }
+    }
+}
