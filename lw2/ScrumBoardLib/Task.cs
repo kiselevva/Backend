db7bd62e7ea3a6b03755c6060c094514ed01600e
kiselevva <80060751+kiselevva@users.noreply.github.com>
2022-06-11 22:55:46 +0300

    Add files via upload

---
+++ lw2/ScrumBoardLib/Task.cs
@@ -0,0 +1,16 @@
+﻿namespace ScrumBoard
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+
+        public Task(string title, string description, Priority priority)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Priority = priority;
+        }
+    }
+}
