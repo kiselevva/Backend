8e6540f4027ae59ce66fd9b8146960eb7ad38d7a
kiselevva <80060751+kiselevva@users.noreply.github.com>
2022-06-11 22:55:13 +0300

    add lw2

---
+++ lw2/ScrumBoardLib/Column.cs
@@ -0,0 +1,46 @@
+namespace ScrumBoard
+{
+    public class Column
+    {
+        private string name;
+        private readonly List<Task> _tasks;
+        public Column(string name)
+        {
+            this.name = name;
+            _tasks = new List<Task>();
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+        public Task RemoveTask(string title)
+        {
+            Task result;
+            bool taskIsFound = false;
+            foreach (Task task in _tasks)
+            {
+                if (task.Title == title)
+                {
+                    taskIsFound = true;
+                    result = task;
+                    _tasks.Remove(task);
+                    return result;
+                }
+            }
+            if (!taskIsFound)
+            {
+                throw new Exception("Task is not found");
+            }
+            return null;
+        }
+        public List<Task> GetTasks()
+        {
+            return _tasks;
+        }
+    }
+}
